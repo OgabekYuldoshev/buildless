17371ace649df6cdfe8ee65037a76de1ffca8b64
Ogabek Yuldoshev <yuldashoff@tutamail.com>
2025-11-30 19:25:56 +0500

    Refactor useBuilder to simplify API by removing rootNodes and allNodes from the returned object, optimizing the useMemo dependencies.

--- packages/react/src/use-builder.ts
+++ packages/react/src/use-builder.ts
@@ -11,20 +11,15 @@ export function useBuilder<
     useCallback(() => builder.getState(), [builder])
   );
 
-  const rootNodes = useMemo(() => indexes.getRootNodes(), [indexes]);
-  const allNodes = useMemo(() => indexes.getAllNodes(), [indexes]);
-
   const api = useMemo(
     () => ({
       indexes,
-      rootNodes,
-      allNodes,
       insert: builder.insert,
       update: builder.update,
       delete: builder.delete,
       move: builder.move,
     }),
-    [indexes, rootNodes, allNodes, builder]
+    [indexes, builder]
   );
 
   return api;
